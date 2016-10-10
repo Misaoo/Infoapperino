b59bf36e478753dd206cc76a5aa68bc181a29d18
TommiSvard <svaerdt@hotmail.com>
2016-10-10 14:24:36 +0200

    Hejhej

---
+++ Twittergalleri/static/app.js
@@ -0,0 +1,30 @@
+/*$( document ).ready(function() {
+    var $img = document.getElementById("img_ul")
+    window.setInterval(function(){
+        $($img).load("/");
+    }, 5000);
+
+
+
+});
+*/
+/* <script>
+      function auto_load(){
+        $.ajax({
+          url: "/",
+          cache: false,
+          success: function(data){
+             $("index").html(data);
+          }
+        });
+      }
+
+      $(document).ready(function(){
+
+        auto_load();
+
+      });
+
+      setInterval(auto_load,5000);
+   </script>
+*/
